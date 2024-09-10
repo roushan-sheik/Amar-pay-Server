5aa64b836c67047f8d193fe2191bb265ed132b11
roushan-sheik <roushansheik@gmail.com>
2024-09-10 21:09:44 +0600

    add: redirect to payment page

--- src/app/modules/payment/payment.utils.ts
+++ src/app/modules/payment/payment.utils.ts
@@ -2,27 +2,35 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const initiatePayment = async () => {
+export const initiatePayment = async (paymentData: any) => {
+  const {
+    transactionId,
+    totalPrice,
+    customerName,
+    customerEmail,
+    customerPhone,
+    customerAddress,
+  } = paymentData;
   const response = await axios.post(process.env.PAYMENT_URL!, {
     store_id: process.env.STORE_ID,
     signature_key: process.env.SIGNATURE_KEY,
-    tran_id: "arifa1234",
+    tran_id: transactionId,
     success_url: "http://www.merchantdomain.com/sucesspage.html",
     fail_url: "http://www.merchantdomain.com/failedpage.html",
     cancel_url: "http://www.merchantdomain.com/cancellpage.html",
-    amount: "10.0",
+    amount: totalPrice,
     currency: "BDT",
     desc: "Merchant Registration Payment",
-    cus_name: "Name",
-    cus_email: "payer@merchantcusomter.com",
-    cus_add1: "House B-158 Road 22",
-    cus_add2: "Mohakhali DOHS",
+    cus_name: customerName,
+    cus_email: customerEmail,
+    cus_add1: customerAddress,
+    cus_add2: "Khulna",
     cus_city: "Dhaka",
     cus_state: "Dhaka",
     cus_postcode: "1206",
     cus_country: "Bangladesh",
-    cus_phone: "+8801704",
+    cus_phone: customerPhone,
     type: "json",
   });
-  console.log(response);
+  return response.data;
 };
