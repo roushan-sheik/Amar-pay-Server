fb82e377ed5d6cd9847b4d6ae4f5a61969f241e8
roushan-sheik <roushansheik@gmail.com>
2024-09-10 19:02:10 +0600

    add: basic setup

--- src/app/config/db.ts
+++ src/app/config/db.ts
@@ -1,17 +1,19 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        // console.log(process.env.DB_URL)
-        await mongoose.connect(process.env.DB_URL as string);
-        console.log('MongoDB Connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
-    }
+  try {
+    // console.log(process.env.DB_URL)
+    await mongoose.connect(process.env.DB_URL as string, {
+      dbName: "arifa-mart",
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
