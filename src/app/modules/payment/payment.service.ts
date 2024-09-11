0caf54bff3ef6339483164196d9758ffb01fdffb
roushan-sheik <roushansheik@gmail.com>
2024-09-11 22:12:03 +0600

    add: payment confirmation message

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,9 +1,12 @@
+import { readFileSync } from "fs";
+import { join } from "path";
 import orderModel from "../order/order.model";
 import { verifyPayment } from "./payment.utils";
 
 const confirmationService = async (transactionId: string, status: string) => {
   const verifyResponse = await verifyPayment(transactionId);
   let result;
+  let message = "";
   if (verifyResponse && verifyResponse.pay_status === "Successful") {
     result = await orderModel.findOneAndUpdate(
       { transactionId },
@@ -11,9 +14,16 @@ const confirmationService = async (transactionId: string, status: string) => {
         paymentStatus: "Paid",
       }
     );
+    message = "Successfully Paid";
+  } else {
+    message = "Payment failed";
   }
+  const filePath = join(__dirname, "../../../view/confirmation.html");
+  console.log("filePath", filePath);
+  let template = readFileSync(filePath, "utf-8");
+  template = template.replace("{{message}}", message);
 
-  return `<h1>Payment ${status}</h1>`;
+  return template;
 };
 export const paymentService = {
   confirmationService,
